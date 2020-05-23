0203d065dee4c975e4e460cc3ed0ee74f7fd678b
Aayush Pal <ap.aayushpal@gmail.com>
2020-05-23 20:43:58 +0530

    added dotenv and gitignore files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import { ApolloServer } from 'apollo-server-express'
-import express from 'express'
-import mongoose from 'mongoose'
-import { resolvers } from './resolvers'
-import { typeDefs } from './typeDefs'
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import mongoose from 'mongoose';
+import { resolvers } from './resolvers';
+import { typeDefs } from './typeDefs';
+require('dotenv').config();
 
 
 const startServer = async () => {
 	
-	const app = express()
+	const app = express();
 
 	const server = new ApolloServer({
 		typeDefs,
@@ -16,7 +17,8 @@ const startServer = async () => {
 	
 	server.applyMiddleware({ app });
 	
-	// replace <connection-string> with your value here
+	// replace <process.env.CONNECTION_STRING> with your value here
+	await mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
 
 	app.listen({ port: 4000 }, () =>
 		console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
